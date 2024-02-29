6288df6028676835a9754e9e343ccf75bfdd3c81
David Airlambang <david.airlambang@gmail.com>
2024-02-29 12:43:51 +0700

    update

--- src/server.ts
+++ src/server.ts
@@ -4,22 +4,21 @@ import express from "express";
 import bookRoutes from "./router/bookRoutes";
 import memberRoutes from "./router/memberRoutes";
 import loanRoutes from "./router/loanRoutes";
-import bodyParser from "body-parser";
 
 // swagger
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../swagger.json";
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.use("/api/v1/books", bookRoutes);
 app.use("/api/v1/members", memberRoutes);
 app.use("/api/v1/loans", loanRoutes);
 
-app.use(express.json());
 app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
